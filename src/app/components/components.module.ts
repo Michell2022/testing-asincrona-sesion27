7e2d555c477a4aff762717dc4a29e19d058fd27f
Michell2022 <sistemas.angel04@gmail.com>
2023-01-26 19:00:56 -0500

    Modulos y componentes creados

---
+++ src/app/components/components.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    LoginComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponentsModule { }
